220d8ed49eec7b0e2ed02028c4780a4a27e3de98
Thiago Lanza <lanzathm@gmail.com>
2021-03-19 17:55:54 -0300

    Candidate service

---
+++ src/app/list-candidates/candidate.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import Candidate from './models/candidate.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CandidateService {
+
+  baseUrl = 'http://127.0.0.1:8000/api/v1';
+
+  constructor(private http: HttpClient) { }
+
+  // List candidates
+  getCandidates(): Observable<Candidate[]> {
+    return this.http.get<Candidate[]>(`${this.baseUrl}/candidates`);
+  }
+
+  // Create candidate
+  postCandidates(candidate: Candidate): Observable<Candidate> {
+    return this.http.post<Candidate>(`${this.baseUrl}/candidates`, candidate);
+  }
+}
