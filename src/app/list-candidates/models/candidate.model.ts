b7f5ed9dcb80a13e7385c60ff40b8b3288b742c0
Thiago Lanza <lanzathm@gmail.com>
2021-03-19 17:55:43 -0300

    Candidate interface

---
+++ src/app/list-candidates/models/candidate.model.ts
@@ -0,0 +1,12 @@
+interface Candidate {
+  id: number,
+  name: string,
+  email: string,
+  age: string,
+  linkedin_url: string,
+  created_at: string,
+  updated_at: string,
+  candidate_technologies: string[]
+}
+
+export default Candidate
